3d3806379586a902fe40c2e948b9cb74a0b2d6d2
tindoann <tindoann@gamil.com>
2020-03-02 19:13:58 -0500

    created components and pages

---
+++ client/src/pages/Blog.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import DeleteBtn from "../components/DeleteBtn";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Books extends Component {
+  state = {
+    blog: []
+  }; 
+
+  componentDidMount() {
+    this.loadBlog(); 
+  }
+
+  loadBlog = () => {
+    API.getBlog()
+    .then(res => this.setState({ blog: res.data }))
+    .catch(err => console(err))
+  }; 
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size='md-6'>
+            <Jumbotron>
+              <h1>Blog List</h1>
+            </Jumbotron>
+            {this.state.blog.length ? (
+              <List>
+                {this.state.blog.map(article => (
+                  <ListItem key={article._id}>
+                    <a href={'/blog/' + article._id}>
+                      <strong>
+                        {article.title} by {article.author}
+                      </strong>
+                    </a>
+                    <DeleteBtn />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    ); 
+  }
+}
+
+export default Blog; 
